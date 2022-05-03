c707cbe8bb657cc2ecd97f876f9062e88bde98d3
Raman Nikitsenka <rnikitsenka@elinext.com>
2022-05-03 16:11:21 +0400

    feat: add first solution

---
+++ 83. Remove Duplicates from Sorted List/0.ts
@@ -0,0 +1,24 @@
+class ListNode {
+  val: number
+  next: ListNode | null
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = (val === undefined ? 0 : val)
+    this.next = (next === undefined ? null : next)
+  }
+}
+
+
+function deleteDuplicates(head: ListNode | null): ListNode | null {
+  let current = head;
+  let prev = null;
+  while (current) {
+    if (prev && current.val === prev.val) {
+      prev.next = current.next;
+    } else {
+      prev = current;
+    }
+    current = current.next;
+  }
+
+  return head
+};
