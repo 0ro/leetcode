713cf5b75abe447d418ed59effb08e0f1587d09a
Raman Nikitsenka <rnikitsenka@elinext.com>
2022-06-21 19:49:27 +0400

    feat: add optimal solution

---
+++ 167. Two Sum II - Input Array Is Sorted/0.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (nums[left] + nums[right] != target) {
+    if (nums[left] + nums[right] > target) {
+      right--;
+    } else {
+      left++
+    }
+  }
+
+  return [left + 1, right + 1];
+};
